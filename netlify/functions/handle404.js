e1f310891d16063d80a2a114d5771b7236f3c7b0
Joel <joel@scriptbased.com>
2025-02-08 09:42:42 -0500

    modified 404 funtion

--- netlify/functions/handle404.js
+++ netlify/functions/handle404.js
@@ -1,8 +1,13 @@
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 404,
-    body: JSON.stringify({
-      message: "404 - Page Not Found",
-    }),
-  };
-};
+import type {Config, Context} from '@netlify/functions'
+
+export default async function(_ : Request, context : Context) {
+  const res = await context.next()
+  return new Response(res.body, {
+    headers: res.headers,
+    status: 404
+  })
+}
+
+export const config : Config = {
+  path: '/404.html' // Change this to your 404 page's path
+}
