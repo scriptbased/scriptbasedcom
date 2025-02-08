a8c19402a54f8fb6eefa580e2a1d4770d5b96ce9
Joel <joel@scriptbased.com>
2025-02-08 00:41:51 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to My Website</h1>
+      </header>
+      <main>
+        <p>This is a simple website hosted on GitHub Pages.</p>
+      </main>
+      <footer>
+        <p>&copy; 2025 My Website</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
