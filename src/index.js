7c96f6bb64e42c36151035297f273357ef13a2b2
Joel <joel@scriptbased.com>
2025-02-09 07:26:03 -0500

    fix slowload url

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import NotFound from './404';
 import ServiceUnavailable from './503';
-import SlowLoad from './SlowLoad';
+import SlowLoad from './slow-load';
 import SlowClientLoad from './SlowClientLoad';
 import './index.css';
 
